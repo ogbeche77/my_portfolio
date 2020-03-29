7287a782ef8beed88fe503334c6ddd97ec39627d
ogbeche77 <ogbeche77@yahoo.com>
2020-03-29 12:32:41 +0200

    DOM items

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,11 @@
 // Select DOM Items
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
+
+//State of the nemu/nav bar
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
